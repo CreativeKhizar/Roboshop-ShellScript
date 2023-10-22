5fd249c83d0f11e47e4bdb85278906bb155ed28c
khizar <creativekhizar1279@gmail.com>
2023-10-22 21:21:15 +0530

    Created Shell Script for Mongodb Server

---
+++ mongodb.sh
@@ -0,0 +1,51 @@
+LOGDIR=/tmp
+DATE=$(date +%F:%H:%M:%S)
+
+LOGFILE=$LOGDIR/$DATE-$0.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e0m"
+
+VALIDATE()
+{
+    if [ $1 -eq 0 ]
+    then
+        echo "$G$2 SUCESS...$N"
+    else
+        echo "$R$2 FAILED...$N"
+    fi
+}
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "$RERROR: Please run the Script with root access$N"
+    exit 1
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "Copying mongo.repo file into repos Directory"
+
+yum install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installing Mongodb Server"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "Enabling mongodb server"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "Starting mongodb Server"
+
+sed -i 's|127.0.0.1|0.0.0.0|g' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "Changing the bind IpAddress from 127.0.0.1 to 0.0.0.0"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "Restarting the mongdb Server"
